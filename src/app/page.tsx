b0c4686c9aafa34ee5d78dac400c140ee6cfb7ec
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-10-29 16:36:34 -0600

    feat: done ?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Image from "next/image";
 import Link from "next/link";
+
+import dionsin from "~/assets/dionsin.webp";
+import moon from "~/assets/moon.webp";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-white flex items-center justify-between absolute left-0 top-0 w-full">
+      <div className="bg-white flex items-center justify-between absolute left-0 top-0 w-full z-10">
         <Link href="/">
           <img src="/logo.svg" alt="DIONSIN" width={130} />
         </Link>
@@ -38,14 +42,26 @@ export default function Home() {
           </li>
         </ul>
       </div>
-      <main className="flex flex-col w-screen h-svh items-center justify-center bg-red-500 gap-8">
-        <h1>Title</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam velit
-          aut veritatis odit nesciunt dolores eveniet voluptatum excepturi
-          doloribus quibusdam assumenda, deleniti dolor debitis, id ut nisi quia
-          suscipit laudantium!
-        </p>
+      <main className="flex w-screen h-svh items-center justify-center gap-8 relative bg-[url('/bg.webp')] bg-cover bg-center bg-no-repeat overflow-hidden text-white">
+        <div className="text-[#cdcccc] font-bold text-6xl uppercase">
+          <p>MIAMI 2024</p>
+          <p className="text-xl">dec</p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src={dionsin}
+            alt="dionsin"
+            width={200}
+            className="-mb-32 z-10"
+          />
+          <Image src={moon} alt="moon" width={500} />
+        </div>
+
+        <div className="text-[#cdcccc] font-bold text-6xl uppercase">
+          <p>MEXICO 2025</p>
+          <p className="text-xl">feb</p>
+        </div>
       </main>
     </>
   );
