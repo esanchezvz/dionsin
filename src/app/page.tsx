03c2574b7c51a2ba076a557c156c2cc1bab8a959
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-10-29 18:37:10 -0600

    feat: make responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,16 @@ import moon from "~/assets/moon.webp";
 export default function Home() {
   return (
     <>
-      <div className="bg-white flex items-center justify-between absolute left-0 top-0 w-full z-10">
+      <div className="bg-white flex items-center justify-between absolute left-0 top-0 w-full z-10 p-4">
         <Link href="/">
-          <img src="/logo.svg" alt="DIONSIN" width={130} />
+          <img
+            src="/logo.svg"
+            alt="DIONSIN"
+            className="w-[100px] md:w-[130px]"
+          />
         </Link>
 
-        <ul className="flex items-center gap-8 pr-6 text-lg">
+        <ul className="flex items-center gap-4 md:gap-8 text-base md:text-lg">
           <li>
             <Link
               className="disabled uppercase text-gray-400 transition-colors cursor-not-allowed"
@@ -42,10 +46,10 @@ export default function Home() {
           </li>
         </ul>
       </div>
-      <main className="flex w-screen h-svh items-center justify-center gap-8 relative bg-[url('/bg.webp')] bg-cover bg-center bg-no-repeat overflow-hidden text-white">
-        <div className="text-[#cdcccc] font-bold text-6xl uppercase">
+      <main className="flex flex-col md:flex-row w-screen h-svh items-center justify-center gap-8 relative bg-[url('/bg.webp')] bg-cover bg-center bg-no-repeat overflow-hidden text-white p-4">
+        <div className="text-[#cdcccc] font-bold text-4xl md:text-6xl uppercase text-center md:text-left">
           <p>MIAMI 2024</p>
-          <p className="text-xl">dec</p>
+          <p className="text-lg md:text-xl">dec</p>
         </div>
 
         <div className="flex flex-col items-center justify-center">
@@ -53,14 +57,19 @@ export default function Home() {
             src={dionsin}
             alt="dionsin"
             width={200}
-            className="-mb-32 z-10"
+            className="-mb-32 z-10 w-[150px] md:w-[200px]"
           />
-          <Image src={moon} alt="moon" width={500} />
+          <Image
+            src={moon}
+            alt="moon"
+            width={500}
+            className="w-[300px] md:w-[500px]"
+          />
         </div>
 
-        <div className="text-[#cdcccc] font-bold text-6xl uppercase">
+        <div className="text-[#cdcccc] font-bold text-4xl md:text-6xl uppercase text-center md:text-left">
           <p>MEXICO 2025</p>
-          <p className="text-xl">feb</p>
+          <p className="text-lg md:text-xl">feb</p>
         </div>
       </main>
     </>
