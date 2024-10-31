4f775408e0d2bf7b8988e1f7fd982564a7d4ec36
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-10-31 10:42:15 -0600

    fix: minor fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${agdasima.variable} ${anton.variable}`}>
+    <html
+      lang="en"
+      className={`${agdasima.variable} ${anton.variable} bg-black`}
+    >
       <body className="antialiased">{children}</body>
     </html>
   );
